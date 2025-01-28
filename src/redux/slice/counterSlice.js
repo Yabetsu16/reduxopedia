4b51df5b71b838172124711dce10b770ac6ffc9f
Yabetsu <it.jabez16@gmail.com>
2025-01-28 12:00:26 +0800

    From Setup To Log State and Dispatch Action

---
+++ src/redux/slice/counterSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { count: 0 };
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    //Define all actions
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export const counterReducer = counterSlice.reducer;
